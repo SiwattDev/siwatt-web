506a7bb59281fbc82d12922dd6fd6433479229f0
Vanortton <siwatt.oficial@gmail.com>
2024-01-26 18:31:12 -0300

    Update 💪

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    // ...
+    apiKey: 'AIzaSyDrEoCFevX0rSXqVdK5KbSIO_ArcEkXfFs',
+    authDomain: 'siwatt-database.firebaseapp.com',
+    projectId: 'siwatt-database',
+    storageBucket: 'siwatt-database.appspot.com',
+    messagingSenderId: '261841563431',
+    appId: '1:261841563431:web:06a4dff00ea63ae231c302',
+    measurementId: 'G-F3P5Z85924',
 }
 
 const app = initializeApp(firebaseConfig)
